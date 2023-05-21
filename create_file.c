427450bb597128e9ff160df86ef47fa4ea3c81be
TomasNagy <tomi.nagy@outlook.com>
2023-05-21 19:31:19 +0200

    Add create_file.c

---
+++ create_file.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+	// Get the user's name
+
+	// Get user's address
+
+	// Check if file exist
+
+	// If the file doesn't exist; create it
+
+	// If file exists write the data into it
+}
